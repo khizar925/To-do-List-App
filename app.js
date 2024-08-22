906323004d50840f787dff3ef21db1cc1bdfc0f5
khizar925 <khizarqamar06@gmail.com>
2024-08-22 23:01:09 +0500

    Add task func added

---
+++ app.js
@@ -0,0 +1,36 @@
+const Data = {
+  taskId: 0,
+  taskText: "",
+  taskStatus: "incomplete",
+};
+
+addTask = () => {
+  Data.taskId++;
+  const text = document.getElementById("input-form");
+  Data.taskText = text.value;
+  const taskContainer = document.querySelector(".tasks");
+  const newTask = document.createElement("div");
+  newTask.innerHTML = `<div class="task-list">
+    <div id="task${Data.taskId}">
+      <span id="Task-${Data.taskId}">${Data.taskText}</span>
+      <button onclick="editTask(${Data.taskId})">Edit</button>
+      <button onclick="deleteTask(${Data.taskId})">Delete</button>
+      <button onclick="completed(${Data.taskId})">Mark as Complete</button>
+      <br><br>
+    </div>
+  </div>`;
+  text.value = "";
+  taskContainer.appendChild(newTask);
+  console.log(Data);
+};
+
+editTask = (taskId) => {
+  const taskSpan = document.getElementById(`Task-${taskId}`);
+  const currentText = taskSpan.textContent;
+  const newText = prompt("Edit Your Task: ", currentText);
+  taskSpan.textContent = newText;
+};
+
+deleteTask = (taskId) => {
+  
+};
